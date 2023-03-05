776aeadc84180db3ef5a75cdf08e56c68e738c58
sooryasrajan <soorya.s27@gmail.com>
2023-03-05 23:55:06 +0530

    Added SVGs for nodes Graph generation with animation in experiment one graph simulation Added iframe load and public code to load the webpage

--- exp-1/src/App.vue
+++ exp-1/src/App.vue
@@ -1,5 +1,7 @@
 <template>
   <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/simulation">Simulation</router-link>
   </nav>
   <router-view/>
 </template>
--- exp-1/src/main.js
+++ exp-1/src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueIframe from 'vue-iframes'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(VueIframe).mount('#app')
